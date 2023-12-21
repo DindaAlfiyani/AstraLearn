8650a4e414ff5622231c226f9a2d5a9713136000
labibbb <syahlabib@gmail.com>
2023-12-21 20:54:12 +0700

    Perubahan

---
+++ AstraLearn/Controllers/SertifikatController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection.Metadata;
+using DocumentITextSharp = iTextSharp.text.Document;
+
+namespace AstraLearn.Controllers
+{
+    public class SertifikatController : Controller
+    {
+        private readonly ILogger<SertifikatController> _logger;
+        BaseFont HeaderFont = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        BaseFont ContentFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        BaseFont TableFont = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+
+        public SertifikatController(ILogger<SertifikatController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GeneratePdf()
+        {
+            // Define a memory stream to store the PDF
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Create a new document with A4 size and landscape orientation
+                DocumentITextSharp doc = new DocumentITextSharp(PageSize.A4.Rotate());
+
+                // Create a PdfWriter instance
+                PdfWriter writer = PdfWriter.GetInstance(doc, memoryStream);
+
+                // Open the document for writing
+                doc.Open();
+
+                // Get the PdfContentByte object from the writer
+                PdfContentByte cb = writer.DirectContent;
+
+                // Adjust the scale and position of the image to fill the entire landscape page
+                var imagePath2 = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets", "SERTIFIKAT.png");
+
+                if (System.IO.File.Exists(imagePath2))
+                {
+                    iTextSharp.text.Image img2 = iTextSharp.text.Image.GetInstance(imagePath2);
+                    img2.ScaleAbsolute(PageSize.A4.Rotate().Width, PageSize.A4.Rotate().Height); // Adjust width and height to match the page
+                    img2.SetAbsolutePosition(0, 0); // Set the position to the bottom-left corner
+                    cb.AddImage(img2);
+                    content(cb);
+                }
+                else
+                {
+                    // Handle the case where the image file doesn't exist
+                    throw new FileNotFoundException("Image file not found: " + imagePath2);
+                }
+
+                // Close the document
+                doc.Close();
+
+                // Return the PDF as a FileContentResult to open in a new tab
+                return File(memoryStream.ToArray(), "application/pdf");
+            }
+        }
+
+        public void content(PdfContentByte cb)
+        {
+            // Title Text
+            cb.BeginText();
+            cb.SetFontAndSize(HeaderFont, 36);
+
+            // Set color and display "Nama Peserta"
+            cb.SetColorFill(new BaseColor(System.Drawing.ColorTranslator.FromHtml("#53AFEF")));
+            cb.ShowTextAligned(PdfContentByte.ALIGN_LEFT, "Nama Peserta", 125, 345, 0);
+
+            // Reset color for subsequent text
+            cb.SetColorFill(new BaseColor(System.Drawing.ColorTranslator.FromHtml("#0D61A8")));
+
+            // Continue displaying other text
+            cb.ShowTextAligned(PdfContentByte.ALIGN_LEFT, "Nama Pelatihan", 125, 255, 0);
+            cb.SetFontAndSize(ContentFont, 14);
+            cb.ShowTextAligned(PdfContentByte.ALIGN_LEFT, "..../AL/NMPLTHN/THNPLTHN", 250, 212, 0);
+            cb.ShowTextAligned(PdfContentByte.ALIGN_LEFT, "20 Desember 2023", 607, 165, 0);
+
+
+            cb.EndText();
+        }
+    }
+}
+
